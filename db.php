c705f80d7977badff1ba24395240f31fc70ed190
Din6819 <67967180+Din6819@users.noreply.github.com>
2022-01-15 08:12:04 +0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+
+<?php
+//db connection
+$db_host = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "iawd2104_nurnabi";
+
+
+$db_connetion = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+?>
